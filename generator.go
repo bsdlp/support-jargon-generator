506e3ae795df69a6017f2f762a06a0fa157f254f
Jon Chen <bsd@voltaire.sh>
2014-01-07 11:31:53 -0400

    init jargon struct

---
+++ generator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+type jargon struct {
+    name    string
+    quote   string
+    source  string
+    date    time.Time
+}
+
+func main() {
+}
+
